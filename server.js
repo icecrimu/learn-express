602f080337a3e673767a5a30dcace79646ffc0a5
icecrimu <charles.banigon@gmail.com>
2022-04-14 13:02:48 +0800

    Redering HTML

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine", "ejs")
+
 app.get("/", (req, res) => {
   console.log("Here")
-  res.json({ message: "Error" })
+  res.render("index", { text: "Goodbye" })
 })
 
 app.listen(3000)
