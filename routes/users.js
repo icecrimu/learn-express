299c06ef062f4bca9eef543bdd262c10e32cc39e
icecrimu <charles.banigon@gmail.com>
2022-04-14 14:30:15 +0800

    Advanced Routing

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,39 @@ router.get("/new", (req, res) => {
   res.send("User New Form")
 })
 
+router.post("/", (req, res) => {
+  res.send("Create User")
+})
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user)
+    res.send(`User Get with Id ${req.params.id}`)
+  })
+  .put((req, res) => {
+    res.send(`Update User with Id ${req.params.id}`)
+  })
+  .delete((req, res) => {
+    res.send(`Delete User with Id ${req.params.id}`)
+  })
+
+// or this
+// router.get("/:id", (req, res) => {
+//   res.send(`User Get with Id ${req.params.id}`)
+// })
+
+// router.put("/:id", (req, res) => {
+//   res.send(`Update User with Id ${req.params.id}`)
+// })
+
+// router.delete("/:id", (req, res) => {
+//   res.send(`Delete User with Id ${req.params.id}`)
+// })
+const users = [{ name: "charles " }, { name: "Jane" }, { name: "Joe" }]
+router.param("id", (req, res, next, id) => {
+  req.user = users[id]
+  next()
+})
+
 module.exports = router
